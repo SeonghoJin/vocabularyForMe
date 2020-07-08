b89ca5871dd43d3a8354d5a947e7b0484b81caef
SeonghoJin <seongho4996@naver.com>
2020-07-08 20:37:26 +0900

    add async process in worddb

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const db = new worddb();
+const word = $qs('#word');
+const desciption = $qs('#wordDescription');
+
+
+console.log(word, desciption);
+console.log(db.getWord());
--- js/popup/worddb.js
+++ js/popup/worddb.js
@@ -6,18 +6,35 @@
     }
 
     worddb.prototype.setWord = function(word,description){
-        let words = this.getWord() || {};
-        words[word] = description;
-        localStorage.setItem(this._dbName,JSON.stringify(words));
+        let _this = this;
+        return new Promise(function(resolve, reject){
+            _this.getWord()
+            .then(function(result){
+                let words = result || {};
+                words[word] = description;
+                localStorage.setItem(_this._dbName,JSON.stringify(words));
+                resolve();
+            })
+        })
     }
 
     worddb.prototype.getWord = function(){
-        return JSON.parse(localStorage.getItem(this._dbName));
+        let _this = this;
+        return new Promise(function(resolve, reject){
+            resolve(JSON.parse(localStorage.getItem(_this._dbName))); //localstorge가 비동기식으로 이루어져있다면 localStorage.getItem의 callback함수 파라미터에 resolve를 넣어줘야 한다.
+        })
     }
 
     worddb.prototype.getDescription = function(word){
-        return this.getWord()[word];
+        let _this = this;
+        return new Promise(function(resolve, reject){
+            _this.getWord().
+            then(function(result){
+                resolve(result[word]);
+            })
+        })
     }
 
     window.worddb = window.worddb || worddb;
+
 })(window)
