9ef685d573c54fa7745a2a2c43cd5e82e8cce08d
SeonghoJin <seongho4996@naver.com>
2020-07-08 19:54:47 +0900

    change worddb

--- js/popup/worddb.js
+++ js/popup/worddb.js
@@ -1,21 +1,23 @@
 'use strict';
 
-module.exports = worddb;
+(function(window){
+    function worddb(){
+        this._dbName = 'words';
+    }
 
-function worddb(){
-    this._dbName = 'words';
-}
+    worddb.prototype.setWord = function(word,description){
+        let words = this.getWord() || {};
+        words[word] = description;
+        localStorage.setItem(this._dbName,JSON.stringify(words));
+    }
 
-worddb.prototype.setWord = function(word,description){
-    let words = this.getWord() || {};
-    words[word] = description;
-    localStorage.setItem(this._dbName,JSON.stringify(words));
-}
+    worddb.prototype.getWord = function(){
+        return JSON.parse(localStorage.getItem(this._dbName));
+    }
 
-worddb.prototype.getWord = function(){
-    return JSON.parse(localStorage.getItem(this._dbName));
-}
+    worddb.prototype.getDescription = function(word){
+        return this.getWord()[word];
+    }
 
-worddb.prototype.getDescription = function(word){
-    return this.getWord()[word];
-}
+    window.worddb = window.worddb || worddb;
+})(window)
