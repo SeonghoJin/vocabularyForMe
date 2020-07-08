14586295714f2f6b4dccc758aaec6e6a56e4bd5f
SeonghoJin <seongho4996@naver.com>
2020-07-08 19:49:12 +0900

    chore : arrange files

--- js/popup/worddb.js
+++ js/popup/worddb.js
@@ -1,15 +1,14 @@
 'use strict';
 
+module.exports = worddb;
+
 function worddb(){
     this._dbName = 'words';
 }
 
 worddb.prototype.setWord = function(word,description){
-
     let words = this.getWord() || {};
-    
     words[word] = description;
-
     localStorage.setItem(this._dbName,JSON.stringify(words));
 }
 
@@ -17,3 +16,6 @@ worddb.prototype.getWord = function(){
     return JSON.parse(localStorage.getItem(this._dbName));
 }
 
+worddb.prototype.getDescription = function(word){
+    return this.getWord()[word];
+}
