82b48d1a16c37d680cda79225029baa8226f1603
SeonghoJin <seongho4996@naver.com>
2020-07-10 08:12:08 +0900

    동적으로 작동하는 페이지에서는 안됨..

--- js/background/excute.js
+++ js/background/excute.js
@@ -1,9 +1,14 @@
+
+
+
 function excute(){
+  /*
    db.getWord()
     .then((words) => {
-      return connect.excuteCode(`let words = ${JSON.stringify(words)}`);
+      return connect.excuteCode(`var words = ${JSON.stringify(words)}`);
     })
     .then(() => {
       return connect.excuteCode('excute()');
     })
+  */
 }
