4c38f5640c12c8874f298dd6d5c4d1ac46dfdbbf
SeonghoJin <seongho4996@naver.com>
2020-07-09 15:56:02 +0900

    impl : datastruct.js

--- js/content/highlight.js
+++ js/content/highlight.js
@@ -3,3 +3,9 @@ function highlights(html,word,desc){
     var regex = new RegExp(`${word}`,'g');
     return html.replace(regex, `<span style="background-color : #ffaaaa" title="${desc}">${word}</span>`);
 }
+
+
+
+function highlights_version2(html,word,desc){
+    const stack = new Stack();
+}
