21ebc0e56d6e333c031be963dbdaf700546fae44
SeonghoJin <seongho4996@naver.com>
2020-07-15 14:39:26 +0900

    $delegate function

--- js/helper.js
+++ js/helper.js
@@ -6,3 +6,24 @@ window.$qs = function(target){
 window.$on = function(target, type, callback, capture){
     target.addEventListener(type,callback,capture);
 }
+
+window.qsa = function (selector, scope) {
+	return (scope || document).querySelectorAll(selector);
+};
+    
+window.$delegate = function (target, selector, type, handler) {
+
+    function dispatchEvent(event) {
+        var targetElement = event.target;
+        var potentialElements = window.qsa(selector, target);
+        var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+
+        if (hasMatch) {
+            handler.call(targetElement, event);
+        }
+    }
+
+    var useCapture = type === 'blur' || type === 'focus';
+
+    window.$on(target, type, dispatchEvent, useCapture);
+};
