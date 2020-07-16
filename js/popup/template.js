1377bc37d14b8eaec07489b3de723d3079d22cd4
SangYeoop <tkdduq8260@gmail.com>
2020-07-16 20:51:52 +0900

    Feat: css 2

--- js/popup/template.js
+++ js/popup/template.js
@@ -9,13 +9,14 @@
         function makeList(words){
             let template = "";
             for(var word in words){
-                template += `<li id="${word}">${word} : ${words[word]} <button class="remove" >삭제</button></li>`
+                template += `<li class="list-group-item list-group-item-action" id="${word}">${word} : ${words[word]} <button class="remove btn btn-outline-danger" >삭제</button></li>`
+
             }
             return template;
         }
         
         let list = makeList(words);
-        return `<ul>${list}</ul>`
+        return `<ul class="list-group">${list}</ul>`
     }
 
     window.template = template;
